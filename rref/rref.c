2709c769561086e9e5e91f9c10ef4bbd810b01b3
takeda <wind.ware1203@gmail.com>
2022-06-03 18:23:59 +0800

    fix rref.c

--- rref/rref.c
+++ rref/rref.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void printM(double** matrix,int m,int n)
+{
+   for( int i = 0; i < m; i++ )
+   {
+      for( int j = 0; j < n; j++ )
+      {  
+         printf( " %2.2lf ",matrix[i][j]);
+      }
+      printf( " \n");
+   }
+   printf( "..................\n");
+}
+void topBest(double** matrix,int m,int n)
+{
+   for(int i = 0;i < m;i++)
+   { 
+      if( matrix[i][0] > matrix [0][0])
+      {
+         for( int j = 0; j < n; j++)
+         {
+             double temp = matrix[i][j];
+             matrix[i][j] = matrix[0][j];
+             matrix[0][j] = temp;
+         }
+      }
+   }     
+}
+
 int main()
 {
 	int m ,n;
@@ -16,22 +45,40 @@ int main()
         matrix[i] = (double* ) malloc(n * sizeof(double ) );
    }
 
+   
+   printf( "Enter the matrix:\n");
    for( int i = 0; i < m; i++ )
    {
       for( int j = 0; j < n; j++ )
-         scanf( "%d",&matrix[i][j]);
-   }
-
-   for( int i = 0; i < m; i++ )
-   {
-      for( int j = 0; j < n; j++ )
-         printf( " %d ",matrix[i][j]);
-      printf( " \n");
+         scanf( "%lf",&matrix[i][j]);
    }
 
 
 
-
+   topBest(matrix, m, n);
+   printM(matrix, m, n);
+   
+   int i = 0, j = 0, k = 0;
+   for(k = 0; k < m - 1; k++)
+   {
+      for(j = k+1; j < n; j++)
+      {  
+         if(matrix[k][k] != 0 )
+            matrix[k][j] = matrix[k][j] / matrix[k][k];
+      }
+      matrix[k][k] = 1.0;
+               
+      for(i = k + 1; i < m; i++)
+      {
+         for(j = k+1; j < n; j++)
+            matrix[i][j] = matrix[i][j] - matrix[i][j] * matrix[k][j];
+         matrix[i][k] = 0.0;
+      }
+      topBest(matrix, m, n);
+      printM(matrix, m, n);
+   }
+   
+   printf( "......DONE........\n");
    free(matrix);
    return 0;
 }
