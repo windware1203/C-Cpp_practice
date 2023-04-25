1cf3d2fac8d1c2dd8fc701a0516ed53fab168733
takeda <wind.ware1203@gmail.com>
2023-04-25 17:28:06 +0800

    make project 5

---
+++ InfoSec/p5/p5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+using namespace std;
+
+
+
+int main()
+{
+    uint32_t arr[] = {1, 1, 2, 6, 24
+                     ,120, 720, 5040, 40320
+                     ,362880, 3628800, 39916800, 479001600};
+
+    cout << "input a integer num:";
+    uint32_t num;
+    cin >> num;
+    cout << ((arr[num-1] % num == (uint32_t)(-1)) ? "Prime" : "Not Prime") ;
+    return 0;
+}
