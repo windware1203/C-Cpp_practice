25ff7015dabb5b5698fdf2f94fee0b0ba4fd3d49
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2023-04-25 19:11:32 +0800

    some fix

--- InfoSec/p5/p5.cpp
+++ InfoSec/p5/p5.cpp
@@ -1,20 +1,38 @@
+/*
+//
+//	Name: Prime number test
+//	Copyright: Takeda Nana - wind.ware1203@gmail.com
+//	Author: Takeda Nana(Cheng-Hao,Zhang)
+//	Date: 25/04/23 19:09
+//
+//	Description: 
+// 	This program implemented by Wilson's theorem for testing the prime number.
+// 	Following is the content:
+// 	A natural number n>1 will be prime if and only if
+// 	(n−1)! + 1≡ 0(mod n) => (n−1)! mod n = n-1
+// 
+// 	This test is not recommended for practical use,
+// 	since the factorial involved rapidly becomes very large. 
+// 	Because 123456789 is not very large, it used in this project.
+//
+*/
 #include <iostream>
 #include <vector>
 #include <cstdint>
+
 using namespace std;
-
-
 
 int main()
 {
-    uint32_t arr[] = {1, 1, 2, 6, 24
+	//
+    uint32_t factorial[] = {1, 1, 2, 6, 24
                      ,120, 720, 5040, 40320
                      ,362880, 3628800, 39916800, 479001600};
 
     cout << "input a integer num:";
     uint32_t num;
     cin >> num;
-    //implemented by Wilson's theorem
-    cout << (((arr[num-1] % num) == (num - 1)) ? "Prime" : "Not Prime") ;
+    
+    cout << (((factorial[num-1] % num) == (num - 1)) ? "Prime" : "Not Prime") ;
     return 0;
 }
