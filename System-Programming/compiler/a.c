a84ce4a45eb89d65a70f41ec096c230bee274b9b
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2023-04-28 18:23:35 +0800

    update

---
+++ System-Programming/compiler/a.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+	printf("hi, i");
+	return 0;
+}
---
+++ System-Programming/compiler/scanner.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define DEFAULT_OUTPUT_FILENAME "output.txt"
+
+int
+main (int argc, char* args[])
+{
+    if(argc != 2 && argc != 4)
+    {
+        printf("usage: %s <input file> <output file>\n", args[0]);
+        return EXIT_SUCCESS;
+    }
+
+
+    FILE* fin = fopen(args[1], "r");
+    if(!fin)
+    {
+        perror("missing input file");
+        return EXIT_FAILURE;
+    }
+
+    const char* output_filename = (argc == 3 ? args[2] : DEFAULT_OUTPUT_FILENAME);
+    FILE* fout = fopen(output_filename, "w");
+    if(!fout)
+    {
+        perror("Fatal error");
+        return EXIT_FAILURE;
+    }
+
+    fclose(fin);
+    fclose(fout);
+    return 0;
+}
