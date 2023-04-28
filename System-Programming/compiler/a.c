88a759e751e358fe5b25d8eedb1df6f668423023
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2023-04-28 22:40:28 +0800

    update

--- System-Programming/compiler/a.c
+++ System-Programming/compiler/a.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-	int a = 10;
+	int a = 10, b = 1;
 	printf("hi, i");
+	printf("%d%d",a,b);
 	return 0;
 }
--- System-Programming/compiler/scanner.c
+++ System-Programming/compiler/scanner.c
@@ -1,9 +1,35 @@
+/**
+ * @file scanner.c
+ * 
+ * @author takedaNana (wind.ware1203@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
+
 #define DEFAULT_OUTPUT_FILENAME "output.txt"
+
+#define TOKEN_SC   0 // single-line comment
+#define TOKEN_MC   1 // multi-line comment
+#define TOKEN_PREP 2 // preprocessor directive
+#define TOKEN_SPEC 3 // special symbol
+#define TOKEN_REWD 4 // reserved word
+#define TOKEN_CHAR 5 // char literal
+#define TOKEN_STR  6 // string literal
+#define TOKEN_FLOT 7 // float
+#define TOKEN_OPER 8 // operator
+#define TOKEN_IDEN 9 // identifier
+#define TOKEN_INTE 10 // interger literal
+
 
 
 
