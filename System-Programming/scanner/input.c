60861f4c112484cc2a4b6e684d129944fbceddf7
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2023-05-26 15:55:13 +0800

    Create input.c

---
+++ System-Programming/scanner/input.c
@@ -0,0 +1,11 @@
+//This is a sample program file for a C-like simple scanner
+main()
+{
+/* this is a multiline comment
+  Hahaha
+  Still another line comment */
+int i, j = 0;
+for (I = 0; I = 100; i++)
+   printf("%d\n", i);
+}
+#include <stdio.h>
