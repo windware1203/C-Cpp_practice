3b8f94ccfb6d54e4f46b220585ba41281b0ab89d
takeda <wind.ware1203@gmail.com>
2023-06-02 13:43:49 +0800

    update

--- System-Programming/scanner/input.c
+++ System-Programming/scanner/input.c
@@ -1,11 +1,7 @@
-//This is a sample program file for a C-like simple scanner
-main()
-{
-/* this is a multiline comment
-  Hahaha
-  Still another line comment */
-int i, j = 0;
-for (I = 0; I = 100; i++)
-   printf("%d\n", i);
-}
 #include <stdio.h>
+
+int main( )
+{
+   int a,b;
+    return 0;
+}
